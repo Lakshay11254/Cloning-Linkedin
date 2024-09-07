a4472af2883af99d3aac876dd6d029166094159f
Lakshay Malhotra <lakshay.sbp@gmail.com>
2024-09-07 15:30:14 -0400

    Implemented Clerk Authenticatin and Done w the Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ClerkProvider } from '@clerk/nextjs'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,17 +25,25 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {/* {Toaster} */}
-   <header>
-     <Header/>
-    </header>    
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className="min-h-screen flex flex-col"
+        >
+          {/* {Toaster} */}
+          <header className="border-b sticky top-0 bg-white z-50">
+            <Header />
+          </header>
+
+          <div className="bg-[#F4F2ED] flex-1 w-full">
+
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,13 @@
-import { SearchIcon } from 'lucide-react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { Briefcase, HomeIcon, MessageSquare, SearchIcon, UsersIcon } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
 
 function Header() {
     return (
-        <div className='flex'>
+        <div className='flex items-center p-2 max-w-6xl mx-auto'>
             <Image
                 className='rounded-lg'
                 src="https://links.papareact.com/b3z"
@@ -13,10 +16,47 @@ function Header() {
                 alt='logo'
             />
 
-            <div>
-                <form>
+            <div className='flex-1'>
+                <form className='flex items-center space-x-1 bg-gray-100 p-2 rounded-md flex-1 mx-2 max-w-96'>
                     <SearchIcon className='text-gray-600 h-4' />
+                    <input
+                        type='text'
+                        placeholder='Search'
+                        className='bg-transparent flex-1 outline-none' />
                 </form>
+            </div>
+
+            <div className='flex items-center space-x-4 px-6' >
+                <Link href="/" className="icon">
+                    <HomeIcon />
+                    <p>Home</p>
+                </Link>
+
+                <Link href="/" className="icon hidden md:flex">
+                    <UsersIcon />
+                    <p>Network</p>
+                </Link>
+
+                <Link href="/" className="icon hidden md:flex">
+                    <Briefcase />
+                    <p>Jobs</p>
+                </Link>
+
+                <Link href="/" className="icon hidden md:flex">
+                    <MessageSquare />
+                    <p>Messagaing</p>
+                </Link>
+
+                <SignedIn>
+                    <UserButton/>
+                </SignedIn>
+
+                <SignedOut>
+                    <Button asChild variant={'outline'}>
+                        <SignInButton/>
+                    </Button>
+                </SignedOut>
+
             </div>
         </div>
     )
