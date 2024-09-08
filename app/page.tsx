cc3802c93c18bee40a746021f864248d4f7ed157
Lakshay Malhotra <lakshay.sbp@gmail.com>
2024-09-07 20:56:46 -0400

    Build the section of UserInformation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
+
+import UserInformation from "@/components/UserInformation";
 
 export default function Home() {
   return (
     <>
-      <div className="flex p-24">
-        Linkedin Clone
-        <Button>Click Me!</Button>
+       <div className="grid grid-cols-8 mt-5 sm:px-5">
+       <section className="hidden md:inline md:col-span-2">
+          <UserInformation/>
+        </section>
+
+        <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
+       
+          {/* PostForm
+              PostFeed */}
+        </section>
+
+        <section className="hidden xl:inline justify-center col-span-2">
+          {/* Widget */}
+        </section>
       </div>
     </>
   );
---
+++ components/PostOptions.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PostOptions() {
+  return (
+    <div>PostOptions</div>
+  )
+}
+
+export default PostOptions
